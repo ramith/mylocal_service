403bea05ba356da82a8a6c67a2e0872b583de0f6
Lakindu Oshadha <59925148+LakinduOshadha@users.noreply.github.com>
2023-07-11 19:36:38 +0530

    Add a warmup function

--- application/api/mylocal/mylocal.py
+++ application/api/mylocal/mylocal.py
@@ -1,10 +1,7 @@
 from flask import Blueprint, Response
+import json
 from application.api.mylocal.controllers.entity_controller import EntityController
 from application.api.mylocal.controllers.table_controller import TableController
-# from application.api.mylocal.geo.geodata import get_region_geo
-
-import json
-
 bp = Blueprint('mylocal', __name__)
 
 @bp.route('/')
@@ -62,3 +59,35 @@ def regions(latlng_str):
     regions = EntityController.get_entity_ids_by_coordinates(latlng_str)
 
     return Response(json.dumps(regions), mimetype='application/json')
+
+def _warmup():
+    """Warmup function."""
+    print('Warming up mylocal service...')
+    
+    EntityController.get_entities('LK-1127015;LK-11;LK-1;LK-1127;EC-01;EC-01C;LG-11031;MOH-11031')
+    TableController('population-gender.regions.2012').get_table_row('LK-1127015')
+    TableController('population-age_group.regions.2012').get_table_row('LK-1127015')
+    TableController('population-marital_status.regions.2012').get_table_row('LK-1127015')
+    TableController('population-ethnicity.regions.2012').get_table_row('LK-1127015')
+    TableController('population-religion.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-lighting.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-cooking_fuel.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-source_of_drinking_water.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-solid_waste_disposal.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-toilet_facilities.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-roof_type.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-floor_type.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-wall_type.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-structure.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-living_quarters.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-type_of_unit.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-occupation_status.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-year_of_construction.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-number_of_rooms.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-number_of_persons.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-relationship_to_head.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-number-of-households.regions.2012').get_table_row('LK-1127015')
+    TableController('social-household-ownership-status.regions.2012').get_table_row('LK-1127015')
+    EntityController.get_entity_ids_by_coordinates('6.9157,79.8636')
+
+    print('mylocal service warmup complete.!')
