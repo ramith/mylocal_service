0643260d01b4673f2ab37e5913142ce835f0a887
Lakindu Oshadha <59925148+LakinduOshadha@users.noreply.github.com>
2023-07-20 17:12:10 +0530

    Add error-handling and logging

--- application/api/mylocal/mylocal.py
+++ application/api/mylocal/mylocal.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, Response
 import json
+from utils.Log import Log
 from application.api.mylocal.controllers.entity_controller import EntityController
 from application.api.mylocal.controllers.table_controller import TableController
 bp = Blueprint('mylocal', __name__)
+log = Log('mylocal')
+
 
 @bp.route('/')
 def index():
     """Index."""
-    data = 'mylocal_service Running.!'
-    return data
+    return f"""mylocal_service Running.!
+     <a href="https://app.swaggerhub.com/apis-docs/LAKINDUOSHADHA98_1/mylocal-service_backend_for_mylocal/1.0.0">
+     API Documentation</a>"""
 
 @bp.route('/entities/<string:entity_ids_str>', methods=['GET'])
 def entity(entity_ids_str):
@@ -16,11 +20,15 @@ def entity(entity_ids_str):
     - Parameters:
         - entity_ids_str (str): A string containing entity IDs separated by semicolons.
     - Returns: JSON response containing entity information for each entity ID.
-    """
-    print('/entities/ entity_ids: ', entity_ids_str)
+    """    
+    log.info(f'/entities/ entity_ids: {entity_ids_str}')
 
-    entity_map = EntityController.get_entities(entity_ids_str)
-    return Response(json.dumps(entity_map), mimetype='application/json')
+    try: 
+        entity_map = EntityController.get_entities(entity_ids_str)
+        return Response(json.dumps(entity_map), mimetype='application/json')
+    except:
+        log.warning(f'/entities/ entity_ids: {entity_ids_str} - Invalid entity ID')
+        return Response(json.dumps({'error': 'Invalid entity ID'}), mimetype='application/json', status=400)
 
 
 @bp.route('/census/<string:table_name>/<string:entity_id>', methods=['GET'])
@@ -31,10 +39,14 @@ def census(table_name,entity_id):
         - entity_id (str): A string representing the entity ID.
     - Returns: JSON response containing census information for the specified entity ID.
     """
-    print('/census/ table_name: ', table_name, 'entity_id: ', entity_id)
+    log.info(f'/census/ table_name: {table_name}, entity_id: {entity_id}')
 
-    census = TableController(table_name).get_table_row(entity_id)
-    return Response(json.dumps(census) ,mimetype='application/json')
+    try:
+        census = TableController(table_name).get_table_row(entity_id)
+        return Response(json.dumps(census) ,mimetype='application/json')
+    except:
+        log.warning(f'/census/ table_name: {table_name}, entity_id: {entity_id} - Invalid table name or entity ID')
+        return Response(json.dumps({'error': 'Invalid table name or entity ID'}), mimetype='application/json', status=400)
 
 @bp.route('/entity/coordinates/<string:entity_id>', methods=['GET'])
 def geo(entity_id):
@@ -42,11 +54,15 @@ def geo(entity_id):
     - Parameters:
         - region_id (str): A string representing the region ID.
     - Returns: JSON response containing geo information for the specified region ID.
-    """
-    print('/entity/coordinates/ entity_id: ', entity_id)
-    coordinates = EntityController.get_coordinates(entity_id)
+    """    
+    log.info(f'/entity/coordinates/ entity_id: {entity_id}')
 
-    return Response(json.dumps(coordinates), mimetype='application/json') 
+    try:
+        coordinates = EntityController.get_coordinates(entity_id)
+        return Response(json.dumps(coordinates), mimetype='application/json')
+    except:
+        log.warning(f'/entity/coordinates/ entity_id: {entity_id} - Invalid entity ID')
+        return Response(json.dumps({'error': 'Invalid entity ID'}), mimetype='application/json', status=400)
 
 @bp.route('/regions/<string:latlng_str>', methods=['GET'])
 def regions(latlng_str):
@@ -55,10 +71,15 @@ def regions(latlng_str):
         - latlng_str (str): latitude,longitude
     - Returns: JSON response containing region IDs for the specified coordinates.
     """
-    print('/regions/ latlng_str: ', latlng_str)
-    regions = EntityController.get_entity_ids_by_coordinates(latlng_str)
+    log.info(f'/regions/ latlng_str: {latlng_str}')
 
-    return Response(json.dumps(regions), mimetype='application/json')
+    try:
+        regions = EntityController.get_entity_ids_by_coordinates(latlng_str)
+        return Response(json.dumps(regions), mimetype='application/json')
+    except:
+        log.warning(f'/regions/ latlng_str: {latlng_str} - Invalid coordinates')
+        return Response(json.dumps({'error': 'Invalid coordinates'}), mimetype='application/json', status=400)
+    
 
 def _warmup():
     """Warmup function."""
